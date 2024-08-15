985fb4182d98d5767a7e5785cf5afd9eb675b65e
Zak <zak@192.168.1.108>
2024-08-16 00:35:41 +0100

    game header file

---
+++ game/Game.h
@@ -0,0 +1,20 @@
+#include "GameComponent.h"
+
+typedef void (*FP)(void);
+
+class Game
+{
+public:
+    Game(int maxComponents);
+    void Add(GameComponent *component);
+    void Run();
+    void SetInitialise(FP init);
+    void SetTerminate(FP term);
+
+private:
+    int componentCount;
+    GameComponent *components;
+    FP initialise;
+    FP terminate;
+    static const int TICKS_1000MS;
+};
